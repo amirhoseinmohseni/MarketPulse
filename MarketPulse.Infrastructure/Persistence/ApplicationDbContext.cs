6a31c4d0dccc34624104b7401494dfb3e27080ce
Amirhosein <312.amirhosein@gmail.com>
2026-05-22 11:25:59 +0330

    add ApplicationDbContext

---
+++ MarketPulse.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MarketPulse.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
