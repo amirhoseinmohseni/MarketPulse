2a96cc7dcfd04975a3082fb520fe369292b1c870
Amirhosein <312.amirhosein@gmail.com>
2026-05-30 21:50:32 +0330

    Add ISearchQueryGenerator

---
+++ MarketPulse.Application/Services/SearchQueryGenerator/AiQueryGenerator.cs
@@ -0,0 +1,17 @@
+﻿using MarketPulse.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPulse.Application.Services.SearchQueryGenerator
+{
+    public class AiQueryGenerator : ISearchQueryGenerator
+    {
+        public Task<List<SearchQuery>> GenerateAsync(string idea, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarketPulse.Application/Services/SearchQueryGenerator/ISearchQueryGenerator.cs
@@ -0,0 +1,11 @@
+﻿using MarketPulse.Domain.Entities;
+
+namespace MarketPulse.Application.Services.SearchQueryGenerator
+{
+    public interface ISearchQueryGenerator
+    {
+        Task<List<SearchQuery>> GenerateAsync(
+            string idea,
+            CancellationToken cancellationToken = default);
+    }
+}
